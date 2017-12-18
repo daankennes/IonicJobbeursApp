4f30212e3b8e08b0b46ec4f0815b287bd53fbbf4
rubenteck <32707826+rubenteck@users.noreply.github.com>
2017-12-18 15:32:07 +0100

    categories added and lay-out tweeks

--- src/pages/company-detail/company-detail.ts
+++ src/pages/company-detail/company-detail.ts
@@ -14,10 +14,26 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class CompanyDetailPage {
 
+  filterNames = [{"name": "Bedrijfsmanagement, Office Management & Communicatie", "color": "#69BB7B"}, {"name": "ICT Multimedia & Grafische en Digitale Media", "color": "#FED035"}, {"name": "Wetenschappen & Techniek", "color": "#FE4C52"}, {"name": "Toegepaste Informatica & Electronica ICT", "color": "#0059ff"}, {"name": "Gezondheid & Onderwijs", "color": "#8E8D93"}];
+  filters: Array<{name: string, color: string }> = [];
+
   company: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.company = this.navParams.data;  }
+	  
+	  this.company = this.navParams.data;
+
+        this.filterNames.forEach(filterName => {
+		  this.company.categories.forEach(color => {
+			if(color === filterName.color){
+			  this.filters.push({
+				name: filterName.name,
+				color: filterName.color
+			  })
+			}
+          });
+        });
+	  }
 
   ionViewDidLoad() {
     console.log(this.company);
